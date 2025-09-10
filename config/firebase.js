0565e500d8e5ae143dd5d1cbde1e75d947209e1d
ibrahim akil <brhimakil.1@gmail.com>
2025-09-10 18:52:59 +0300

    added

--- config/firebase.js
+++ config/firebase.js
@@ -15,7 +15,7 @@ const initializeFirebase = () => {
       "auth_uri": "https://accounts.google.com/o/oauth2/auth",
       "token_uri": "https://oauth2.googleapis.com/token",
       "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-      "client_x509_cert_url": process.env.FIREBASE_CLIENT_CERT_URL
+      "client_x509_cert_url": `https://www.googleapis.com/robot/v1/metadata/x509/${process.env.FIREBASE_CLIENT_EMAIL}`
     };
 
     admin.initializeApp({
